9e49a5355a112cff78958cb914985fe70f81bcbf
yurizou <daikiiiiiiin777@gmail.com>
2023-05-14 05:31:32 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
 def index
   @book = Book.new
   @books = Book.all
@@ -19,8 +19,17 @@ def edit
   @book =Book.find(params[:id])
 end
 
+def update
+  book = Book.find(params[:id])
+  book.update(book_params)
+  redirect_to book_path(book.id)
+
+end
+
 def destroy
-  
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to '/books'
 end
 
 private
