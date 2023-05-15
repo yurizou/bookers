606f9df1a01aac292b1c0487e7e12e888b5e1750
yurizou <daikiiiiiiin777@gmail.com>
2023-05-15 14:03:20 +0000

    [Add]varidatesの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,19 @@
 class BooksController < ApplicationController
 
 def index
-  @book = Book.new
-  @books = Book.all
+   @book = Book.new
+   @books = Book.all
 end
 
 def create
-  book = Book.new(book_params)
-  if book.save
+  @book = Book.new(book_params)
+  if @book.save
   flash[:notice] = "Book was successfully created."
-  redirect_to book_path(book.id)
-else
-  render :
+  redirect_to book_path(@book.id)
+  else
+  @books = Book.all
+  render :index
+  end
 end
 
 def show
@@ -23,11 +25,14 @@ def edit
 end
 
 def update
-  book = Book.find(params[:id])
-  book.update(book_params)
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
   flash[:notice] = "Book was successfully updated."
-  redirect_to book_path(book.id)
+  redirect_to book_path(@book.id)
+  else
 
+  render :edit
+  end
 end
 
 def destroy
