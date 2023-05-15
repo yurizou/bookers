195ccc3eb9920c7281af97233c800dd0700691d2
yurizou <daikiiiiiiin777@gmail.com>
2023-05-15 12:57:18 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,11 @@ end
 
 def create
   book = Book.new(book_params)
-  book.save
+  if book.save
+  flash[:notice] = "Book was successfully created."
   redirect_to book_path(book.id)
+else
+  render :
 end
 
 def show
@@ -22,6 +25,7 @@ end
 def update
   book = Book.find(params[:id])
   book.update(book_params)
+  flash[:notice] = "Book was successfully updated."
   redirect_to book_path(book.id)
 
 end
